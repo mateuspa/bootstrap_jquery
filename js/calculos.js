aa656a247a780f653c32aefb1dbf3b6fdfdaf58d
Mateus Prince <mateus.prince@gmail.com>
2025-05-31 12:01:55 -0300

    atualizando cálculos

--- js/calculos.js
+++ js/calculos.js
@@ -11,71 +11,24 @@ $("#calcular").click(function () {
         return;
     }
 
-    let calculo = adicionais.toLocaleString('pt-BR',
-        {
-            style: 'currency',
-            currency: 'BRL',
-            minimumFractionDigits: 2
-        });
+    // calculando adicional de 8% INSS
+    inss = salario * 0.08;
+
+    // calculando adicional de 5% Vale Transporte
+    vale = salario * 0.05;
+
+    if (idade > 50) {
+        bonus = 300;
+    } else {
+        bonus = 200;
+    }
+
+    salario_liquido = salario - inss - vale + bonus;
 
     if (idade > 1) {
-        alert("Nome do funcionário: " + nome+"\nNúmero de dependentes: " + dependentes+"\nSalário Bruto: " + salario +"\nINSS: " + salario+"\nVale Transporte: " + salario+"\nSalário Líquido: " + calculo)
-
-            /*
-            
-            + salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + '\nINSS: ' + (salario * 0.11).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + '\nVale Transporte: ' + (salario * 0.06).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' };
-*/
+        alert("Nome do funcionário: " + nome + "\nNúmero de dependentes: " + dependentes + "\nSalário Bruto: " + salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nINSS: " + inss.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nVale Transporte: " + vale.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nSalário Líquido: " + salario_liquido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
     } else {
         alert('Valor menor ou igual a 6: ' + adicionais)
-        
+
     }
-/*
-    // if ternário: decisão em uma linha
-    //var mostrar = "da média"
-    //mostrar = (media > 6) ? "acima " + mostrar : "abaixo " + mostrar;
-    //alert('Resultado do if ternário: ' + mostrar)
-
-    //estrutura de decisão - switch case
-    var msg = "A média é: ";
-    switch (parseInt(media) % 2) {
-        case 0:
-            msg += "PAR";
-            break;
-        case 1:
-            msg += "IMPAR";
-            break;
-        default:
-            msg = "erro";
-            break;
-    }
-    alert(msg)
-
-    switch (true) {
-        case (media > 10):
-            msg = "Maior que 10";
-            break;
-        case (media > 5):
-            msg = "Maior que 5";
-            break;
-        default:
-            msg = "Menor ou igual a 5";
-            break;
-    }
-    alert(msg)
-
-
-    //fatorial
-    var fatorial = 1;
-
-
-    for (let i = 1; i <= v1; i++) {
-        fatorial *= i;
-    }
-
-    alert('O valor fatorial de v1 é: ' + fatorial)
-
-    $("#txtfatorial")
-        .text("Valor do fatorial é: " + fatorial)
-        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': '#00f' });
-        */	
 });
