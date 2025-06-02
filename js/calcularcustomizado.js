5c833e86d24f83e54ea6f64801e440ab8925978f
Mateus Prince <mateus.prince@gmail.com>
2025-06-02 01:29:54 -0300

    melhorando visual

--- js/calcularcustomizado.js
+++ js/calcularcustomizado.js
@@ -1,36 +1,33 @@
-$("#calcular").click(function () {
+$("#calcularcustomizado").click(function () {
     let nome = $("#nome").val();
     let idade = parseFloat($("#idade").val());
-    let salario = parseFloat($("#salario").val());
+    let salariobruto = parseFloat($("#salariobruto").val());
     let dependentes = parseFloat($("#dependentes").val());
     let aliquotainss = parseFloat($("#aliquotainss").val());
     let aliquotafgts = parseFloat($("#aliquotafgts").val());
     let aliquotavaletransporte = parseFloat($("#aliquotavaletransporte").val());
+    let bonus = parseFloat($("#bonus").val());
 
-    if (isNaN(idade) || isNaN(salario) ) {
+    if (isNaN(idade) || isNaN(salariobruto) ) {
         alert('Preencher campos obrigatórios: Idade e Salário Bruto.')
         return;
     }
 
     // calculando adicional de INSS
-    inss = salario * (aliquotainss / 100);
+    inss = salariobruto * (aliquotainss / 100);
     
     // calculando adicional de FGTS
-    fgts = salario * (aliquotafgts / 100);
+    fgts = salariobruto * (aliquotafgts / 100);
 
     // calculando adicional de Vale Transporte
-    valetransporte = salario * (aliquotavaletransporte / 100);
+    valetransporte = salariobruto * (aliquotavaletransporte / 100);
 
-    // calculando bônus
-    bonus = 0;
-    
     //calculado salário líquido
-    salarioliquido = salario - inss - fgts -valetransporte + bonus;
+    salarioliquido = salariobruto - inss - fgts -valetransporte + bonus;
 
     if (idade > 1) {
-        alert("Nome do funcionário: " + nome + "\nNúmero de dependentes: " + dependentes + "\nSalário Bruto: " + salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nINSS: " + inss.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nVale Transporte: " + vale.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nSalário Líquido: " + salario_liquido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+      alert("Nome do funcionário: " + nome + "\nNúmero de dependentes: " + dependentes + "\nSalário Bruto: " + salariobruto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nINSS: " + inss.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nFGTS: " + fgts.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nVale Transporte: " + valetransporte.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nBônus: " + bonus.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + "\nSalário Líquido: " + salarioliquido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
     } else {
         alert('Baby detectado, não tem salário.')
-
     }
 });
